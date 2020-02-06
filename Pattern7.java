145a2433e1a36a73385d3914b56ffd27620fc441
Anita <er.anitapal@gmail.com>
2020-02-06 15:10:24 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,75 @@
+class Pattern7
+{ 
+   public static void main(String[] args)
+    {
+	   int n = 5;
+	   int k = 1;
+		char ch = 'a';
+	  for(int i=0;i<n;i++)
+	  {
+	    
+	    for(int j=0;j<n;j++)
+		 { 
+		 if((i+j) % 2== 0)
+		 {
+		  System.out.print(k++ +" ");
+		 
+		 
+		 
+		 }
+		 else
+		 {
+		   System.out.print(ch++ +" ");
+		 }
+		 
+		}
+	  
+        System.out.println(" ");	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
